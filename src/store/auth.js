2e88700320ddad2684f1000e06093fe7322b565d
Ezhov Sergey <serga.ezhov.5@gmail.com>
2020-10-21 21:09:58 +0600

    saving change

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,26 +1,28 @@
-export default {
-  actions: {
-    async fetch_loginInfo ({ commit }) {
-      const res = await fetch('http://localhost:8081/test', {
-        mode: 'no-cors'
-      })
-      console.log(res)
-      const loginInfo = await res.json()
-      console.log(loginInfo)
-      commit('update_loginInfo', loginInfo)
-    }
-  },
-  mutations: {
-    update_loginInfo (state, loginInfo) {
-      state.loginInfo = loginInfo
-    }
-  },
-  state: {
-    loginInfo: []
-  },
-  getters: {
-    all_loginInfo (state) {
-      return state.loginInfo
-    }
-  }
-}
+// import axios from 'axios'
+// export default {
+//   actions: {
+//     fetch_loginInfo () {
+//       const config = {
+//         headers: {
+//           mode: 'no-cors'
+//         }
+//       }
+//       axios
+//         .get('http://localhost:8081/test', config)
+//         .then(response => (this.loginInfo = response))
+//     }
+//   },
+//   mutations: {
+//     update_loginInfo (state, loginInfo) {
+//       state.loginInfo = loginInfo
+//     }
+//   },
+//   state: {
+//     loginInfo: []
+//   },
+//   getters: {
+//     all_loginInfo (state) {
+//       return state.loginInfo
+//     }
+//   }
+// }
--- src/views/TestDelite.vue
+++ src/views/TestDelite.vue
@@ -1,21 +1,38 @@
 <template>
   <div id="testDelite">
-    <div class='post' v-for="post in all_loginInfo" :key="post.to">
-      <p>{{post.to}}</p>
-      <p>{{post.from}}</p>
-      <p>{{post.message}}</p>
-    </div>
+    {{posts}}
   </div>
 </template>
 
 <script> // Удалить страницу, создавалась для тестов
-import { mapGetters, mapActions } from 'vuex'
+import axios from 'axios'
 export default {
-  name: 'testDelite',
-  computed: mapGetters(['all_loginInfo']),
-  methods: mapActions(['fetch_loginInfo']),
-  async mounted () {
-    this.fetch_loginInfo()
+  data: () => ({
+    posts: null
+  }),
+  created () {
+    const config = {
+      headers: {
+        mode: 'no-cors'
+      }
+    }
+    axios
+      .get('http://localhost:8081/test', config)
+      .then(response => (this.posts = response.data))
   }
 }
+// const config = {
+//   headers: {
+//     mode: 'no-cors'
+//   }
+// }
+// import { mapGetters, mapActions } from 'vuex'
+// export default {
+//   name: 'testDelite',
+//   computed: mapGetters(['all_loginInfo']),
+//   methods: mapActions(['fetch_loginInfo']),
+//   async mounted () {
+//     this.fetch_loginInfo()
+//   }
+// }
 </script>
